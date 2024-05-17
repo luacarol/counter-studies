a2305bc231aca8b74bbb51cce3dbd6da5bd3e635
luanacaroliny <luana.oliveira@autaza.com>
2024-05-16 21:34:51 -0300

    Creating form

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Button from './components/Button/index';
+import Form from './components/Form';
 
 function App() {
   return (
     <div className="App">
       <Button/>
+      <Form/>
     </div>
   );
 }
