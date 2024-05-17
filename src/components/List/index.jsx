4314ad796dc7966139d9e05931a1b4b56cd406d7
luanacaroliny <luana.oliveira@autaza.com>
2024-05-16 21:45:28 -0300

    Dynamically rendered arrays

--- src/components/List/index.jsx
+++ src/components/List/index.jsx
@@ -1,24 +1,22 @@
 function List() {
+    const tasks = [{
+        name: 'React',
+        time: '02:00:00'
+    }, {
+        name: 'Javascript',
+        time: '01:00:00'
+    }]
+
     return (
         <aside>
             <h2>Estudos do dia</h2>
             <ul>
-                <li>
-                    <h3>
-                        React
-                    </h3>
-                    <span>
-                        02:00:00
-                    </span>
-                </li>
-                <li>
-                    <h3>
-                        Javascript
-                    </h3>
-                    <span>
-                        01:00:00
-                    </span>
-                </li>
+                {tasks.map((task, index) => (
+                    <li key={index}>
+                        <h3>{task.name}</h3>
+                        <span>{task.time}</span>
+                    </li>
+                ))}
             </ul>
         </aside>
     )
