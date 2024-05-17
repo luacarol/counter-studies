2ac66f8e2afa85996eeda45fd4ce60fba4b68e7e
luanacaroliny <luana.oliveira@autaza.com>
2024-05-16 21:39:55 -0300

    Creating list

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Button from './components/Button/index';
 import Form from './components/Form';
+import List from './components/List';
 
 function App() {
   return (
     <div className="App">
       <Button/>
       <Form/>
+      <List/>
     </div>
   );
 }
