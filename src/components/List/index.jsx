1b3a9874f8a91fd8d56ce2a2e953c099472f1efd
Luana Caroliny <luana.oliveira@autaza.com>
2024-05-23 13:21:14 -0300

    Using module in css files

--- src/components/List/index.jsx
+++ src/components/List/index.jsx
@@ -1,4 +1,4 @@
-import './style.scss';
+import style from './List.module.scss';
 
 function List() {
     const tasks = [{
@@ -10,11 +10,11 @@ function List() {
     }]
 
     return (
-        <aside className='listaTarefas'>
+        <aside className={style.listaTarefas}>
             <h2>Estudos do dia</h2>
             <ul>
                 {tasks.map((task, index) => (
-                    <li key={index} className='item'>
+                    <li key={index} className={style.item}>
                         <h3>{task.name}</h3>
                         <span>{task.time}</span>
                     </li>
